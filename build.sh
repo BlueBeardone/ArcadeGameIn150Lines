42dd7b5b5c9d1668b6e7bd3ea23470cdae75df21
BlueBeardone <barend.army@gmail.com>
2025-09-03 14:13:18 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 pip install pygame pygbag
 pygbag --build snake_game.py
-cp index.html build/web
+mkdir -p itch_build
+cp -r build/web/* itch_build/
+cp index.html itch_build/
+cd itch_build && zip -r ../snake_game_itch.zip . && cd ..
