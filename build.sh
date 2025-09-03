02995a0c98b741d51041cabf7d676165efb137a5
BlueBeardone <barend.army@gmail.com>
2025-09-03 14:44:47 +0200

    Made some updates

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pip install pygame pygbag
 pygbag --build snake_game.py
-cp index.html build/web/
+cp "$(python -c "import pygbag; import os; print(os.path.join(os.path.dirname(pygbag.__file__), 'pygbag.js'))" build/web/
+cp -r assets/ build/web/ 2>/dev/null || true
